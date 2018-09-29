349487792504ea26fce36ad9a8f8b9911297eb7a
Marina Barros <marina.barros@acad.pucrs.br>
2018-09-29 18:15:27 -0300

    Implemeta interface

---
+++ src/JogoInterface.java
@@ -0,0 +1,20 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface JogoInterface extends Remote {
+
+    int registraJogador(String nomeJogador) throws RemoteException;
+
+    int encerraPartida(int idUsuario) throws RemoteException;
+
+    int teamPartida(int idUsuario) throws RemoteException;
+
+    String obtemOponente(int idUsuario) throws RemoteException;
+
+    int ehMinhaVez(int idUsuario) throws RemoteException;
+
+    String obtemTabuleiro(int idUsuario) throws RemoteException;
+
+    int movePeca(int idUsuario, int linha, int coluna, int direcao) throws RemoteException;
+
+}
